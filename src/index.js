eb357d3d1919372b2095845b6cd5c592c812a05f
Progrema Studio <progrema.studio@gmail.com>
2019-04-05 06:13:00 +0700

    Use express to serve static page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send({
-    message: "Hello World"
-  });
-});
+const publicDirectoryPath = path.join(__dirname, "../public");
+app.use(express.static(publicDirectoryPath));
 
 app.listen(port, () => {
   console.log(`Server is up at port ${port} on ${new Date()}`);
