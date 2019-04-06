762d51b1d0df4b04908f5526d7635e9d47633390
Progrema Studio <iqbalpakeh@gmail.com>
2019-04-06 10:32:30 +0700

    Clean code

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,10 +6,8 @@ socket.on("message", message => {
 
 document.querySelector("#message-form").addEventListener("submit", e => {
   e.preventDefault();
-
   const message = e.target.elements.message.value;
   socket.emit("sendMessage", message);
-
   e.target.elements.message.value = "";
 })
 
@@ -17,7 +15,6 @@ document.querySelector("#send-location").addEventListener("click", e => {
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser");
   }
-
   navigator.geolocation.getCurrentPosition(position => {
     console.log(position);
   })
