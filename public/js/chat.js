fed31f2ab7ea0b6db045ad762a35510c0a735433
Progrema Studio <iqbalpakeh@gmail.com>
2019-04-06 21:39:18 +0700

    Add comment

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,29 @@
 const socket = io();
 
+/**
+ * Elements
+ */
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+
+/**
+ * Handle message coming from server
+ */
 socket.on("message", message => {
   console.log(message);
 })
 
-document.querySelector("#message-form").addEventListener("submit", e => {
+/**
+ * Handle submit event to send message
+ */
+$messageForm.addEventListener("submit", e => {
   e.preventDefault();
+
+  // disable button after press button
+  $messageFormButton.setAttribute("disabled", "disabled");
+
+  // send message to server
   const message = e.target.elements.message.value;
   socket.emit("sendMessage", message, error => {
     if (error) {
@@ -13,13 +31,22 @@ document.querySelector("#message-form").addEventListener("submit", e => {
     }
     console.log("Message delivered!");
   });
+
+  // clear edit text box
   e.target.elements.message.value = "";
 })
 
+/**
+ * Handle send location to server event
+ */
 document.querySelector("#send-location").addEventListener("click", e => {
+
+  // Check if geolocatino supported by browser
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser");
   }
+
+  // Get current position 
   navigator.geolocation.getCurrentPosition(position => {
     socket.emit("sendLocation", {
       latitude: position.coords.latitude,
