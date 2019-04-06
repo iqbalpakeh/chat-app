5bc5a88c0b70d11e8de95ae15c3724326e61e9d6
Progrema Studio <iqbalpakeh@gmail.com>
2019-04-06 21:52:30 +0700

    Disable button while sending location

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,6 +6,7 @@ const socket = io();
 const $messageForm = document.querySelector("#message-form");
 const $messageFormInput = $messageForm.querySelector("input");
 const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
 
 /**
  * Handle message coming from server
@@ -18,41 +19,41 @@ socket.on("message", message => {
  * Handle submit event to send message
  */
 $messageForm.addEventListener("submit", e => {
-  e.preventDefault();
 
-  // disable button after press button
+  e.preventDefault();
   $messageFormButton.setAttribute("disabled", "disabled");
 
-  // send message to server
-  const message = e.target.elements.message.value;
-  socket.emit("sendMessage", message, error => {
+  socket.emit("sendMessage", $messageFormInput.value, error => {
+
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
     if (error) {
       return console.log(error);
     }
     console.log("Message delivered!");
   });
-
-  // clear edit text box
-  e.target.elements.message.value = "";
 })
 
 /**
  * Handle send location to server event
  */
-document.querySelector("#send-location").addEventListener("click", e => {
+$sendLocationButton.addEventListener("click", e => {
 
-  // Check if geolocatino supported by browser
+  $sendLocationButton.setAttribute("disabled", "disabled");
+
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser");
   }
 
-  // Get current position 
   navigator.geolocation.getCurrentPosition(position => {
     socket.emit("sendLocation", {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     }, ack => {
       console.log(ack);
+      $sendLocationButton.removeAttribute("disabled");
     });
   })
 })
