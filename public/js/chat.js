938d0ba2052b4591043bb9e4aa5c948c730f3e57
Progrema Studio <iqbalpakeh@gmail.com>
2019-04-06 09:42:59 +0700

    Remove comment

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,9 @@
 const socket = io();
 
-/**
- * Handle message event
- */
 socket.on("message", message => {
   console.log(message);
 })
 
-/**
- * Handle click message event
- */
 document.querySelector("#message-form").addEventListener("submit", e => {
   e.preventDefault();
   const message = e.target.elements.message.value;
