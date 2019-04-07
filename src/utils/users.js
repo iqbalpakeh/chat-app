b8f2639448270bb6b8295f3de4ff859b19d1cd37
Progrema Studio <iqbalpakeh@gmail.com>
2019-04-07 12:23:52 +0700

    Export function

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,10 +1,5 @@
 const users = [];
 
-/**
- * Add user to list 
- * 
- * @param {*} param0 user object 
- */
 const addUser = ({ id, username, room }) => {
   // Clean the data
   username = username.trim().toLowerCase();
@@ -35,11 +30,6 @@ const addUser = ({ id, username, room }) => {
   return { users };
 }
 
-/**
- * Remove user from list
- * 
- * @param {*} id of user
- */
 const removeUser = id => {
   const index = users.findIndex(user => user.id === id);
   if (index !== -1) {
@@ -47,49 +37,18 @@ const removeUser = id => {
   }
 }
 
-/**
- * get user by id
- * 
- * @param {*} id of user
- */
 const getUser = id => {
-  const index = users.findIndex(user => user.id === id);
-  return users[index];
+  return users.find(user => user.id === id);
 }
 
-/**
- * get all user in room
- * 
- * @param {*} room of users
- */
 const getUsersInRoom = room => {
   room = room.trim().toLowerCase();
-  const usersInRoom = users.filter(user => user.room === room);
-  return usersInRoom;
+  return users.filter(user => user.room === room);
 }
 
-// ------------ Test!!
-
-addUser({
-  id: 22,
-  username: "Iqbal   ",
-  room: "Batam"
-});
-
-addUser({
-  id: 23,
-  username: "Aziza   ",
-  room: "Singapore"
-});
-
-addUser({
-  id: 24,
-  username: "Safiya   ",
-  room: "Batam"
-});
-
-console.log(users);
-
-console.log(getUser(23))
-
-console.log(getUsersInRoom("Batam"));
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+}
